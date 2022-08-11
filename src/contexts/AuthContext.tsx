db663fe46d452006b7e9b5b085251de7cdf6b5d0
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-08-11 11:16:32 -0300

    AuthContex

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,1 +1,15 @@
 import React, {useState, createContext } from "react"; 
+
+type AuthContextData = {
+    user: AuthContextProps,
+    isAthenticated: boolean,
+}
+
+type AuthContextProps = {
+    id: string,
+    name: string,
+    email: string,
+    token: string,
+}
+
+export const AuthContext = createContext({})
